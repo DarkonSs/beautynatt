3d62a211d7f4c47c32df1efe2682b7a9ced87e50
DarkonS <zrdarkon@gmail.com>
2023-02-01 18:52:16 +0100

    lastUpdate3

--- javascript/mainScripts.js
+++ javascript/mainScripts.js
@@ -101,3 +101,132 @@ if (code3) {
         </div>
 `
 }
+
+function hambmenu() {
+  var x = document.getElementById("section");
+  var y = document.querySelector(".container");
+  var body = document.querySelector("body")
+  var header = document.querySelector("header")
+
+  if (x.className.includes("hidden-section")) {
+      x.classList.remove('hidden-section');
+      x.classList.add("slide-left");
+      x.style.position = 'fixed'
+      y.classList.add("disable-selection")
+      y.style.paddingTop = '79px'
+      header.style.position = 'fixed'
+      header.style.width = '100%'
+      document.querySelector("#nav-icon3").classList.add("open");
+      body.style.overflow = 'hidden'
+
+  } else {
+      x.classList.add("slide-right");
+      x.classList.remove('slide-left');
+      setTimeout(function () {
+          x.classList.add('hidden-section');
+          x.classList.remove('slide-right');
+      }, 300);
+      y.classList.remove("disable-selection")
+      document.querySelector("#nav-icon3").classList.remove("open");
+      y.style.paddingTop = 'inherit'
+      header.style.position = 'inherit'
+      header.style.width = 'inherit'
+      body.style.overflow = 'inherit'
+  }
+
+  if (x.className.includes("slide-left")) {
+      document.addEventListener('click', function handleClickOutsideBox(event) {
+
+          if (event.target == body) {
+              y.classList.remove("disable-selection")
+              document.querySelector("#nav-icon3").classList.remove("open");
+              y.style.paddingTop = 'inherit'
+              header.style.position = 'inherit'
+              header.style.width = 'inherit'
+              body.style.overflow = 'inherit'
+              x.classList.add("slide-right");
+              x.classList.remove('slide-left');
+              setTimeout(function () {
+                  x.classList.add('hidden-section');
+                  x.classList.remove('slide-right');
+              }, 300);
+          }
+      });
+
+  }
+}
+
+window.addEventListener("resize", function () {
+  const mediaQuery2 = window.matchMedia("(min-width: 1200px)");
+
+  const applyStyles2 = () => {
+
+      if (mediaQuery2.matches) {
+
+          var x = document.getElementById("section");
+          var y = document.querySelector(".container");
+
+          if (!x.className.includes("hidden-section")) {
+
+              document.querySelector("#nav-icon3").classList.remove("open");
+
+              x.classList.add("slide-right");
+              x.classList.remove('slide-left');
+              setTimeout(function () {
+                  x.classList.add('hidden-section');
+                  x.classList.remove('slide-right');
+              }, 300);
+              y.classList.remove("disable-selection")
+          }
+
+      }
+  };
+
+  applyStyles2();
+
+  mediaQuery2.addListener(applyStyles2);
+});
+
+document.addEventListener('scroll', function () {
+  if (document.getElementById('header')) {
+      if (window.pageYOffset <= document.getElementById('header').offsetHeight + 40) {
+
+          document.getElementById('bubtn').style.display = 'none'
+
+      } else if (document.getElementById('footer').style.display != 'none') {
+
+          if (window.pageYOffset + window.innerHeight >= document.getElementById('footer').offsetTop) {
+
+              document.getElementById('bubtn').style.display = 'none'
+
+          } else {
+
+              document.getElementById('bubtn').style.display = 'inherit'
+
+          }
+      } else {
+          document.getElementById('bubtn').style.display = 'inherit'
+      }
+  }
+
+  if (document.getElementById('header2')) {
+      if (window.pageYOffset <= document.getElementById('header2').offsetHeight + 40) {
+
+          document.getElementById('bubtn').style.display = 'none'
+
+      } else if (document.getElementById('footer').style.display != 'none') {
+
+          if (window.pageYOffset + window.innerHeight >= document.getElementById('footer').offsetTop) {
+
+              document.getElementById('bubtn').style.display = 'none'
+
+          } else {
+
+              document.getElementById('bubtn').style.display = 'inherit'
+
+          }
+      } else {
+          document.getElementById('bubtn').style.display = 'inherit'
+      }
+  }
+})
--- javascript/moreContentElement.js
+++ javascript/moreContentElement.js
@@ -43,3 +43,13 @@ window.addEventListener("resize", function () {
 
     mediaQuery5.addListener(applyStyles5);
 });
+
+function leftScroll(ele) {
+    const left = ele.parentNode.children[1]
+    left.scrollBy(-270, 0);
+}
+
+function rightScroll(ele) {
+    const right = ele.parentNode.children[1]
+    right.scrollBy(270, 0);
+}

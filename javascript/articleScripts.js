f9db6a41c7b609de98595035dbc535ddef8a1698
DarkonS <zrdarkon@gmail.com>
2023-02-02 17:59:19 +0100

    newUpdate

--- javascript/articleScripts.js
+++ javascript/articleScripts.js
@@ -230,25 +230,3 @@ document.addEventListener('scroll', function () {
       }
   }
 })
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCz61uJxWIZpi731gEQwIN6CTYwsPxjaEo",
-  authDomain: "beautynatt-95095.firebaseapp.com",
-  projectId: "beautynatt-95095",
-  storageBucket: "beautynatt-95095.appspot.com",
-  messagingSenderId: "823311186042",
-  appId: "1:823311186042:web:f1533408d157d14c6f63c3",
-  measurementId: "G-BXFSQZ8HZ6"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
